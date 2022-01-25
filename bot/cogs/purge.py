46c048312c7ea667148cf63c99d1f29d4fa6c752
parisap06 <parisap06@gmail.com>
2022-01-25 15:52:39 +0200

    Updated warn.py

--- bot/cogs/purge.py
+++ bot/cogs/purge.py
@@ -1,5 +1,6 @@
 import discord
 from discord.ext import commands
+import asyncio
 
 class Purge(commands.Cog):
 
@@ -8,11 +9,35 @@ class Purge(commands.Cog):
 
     @commands.command()
     @commands.has_permissions(manage_messages=True)
-    async def purge(self, ctx, amount=5):
-     await ctx.message.delete()
-     await ctx.channel.purge(limit=amount)
-     await ctx.send(f'**{amount} messages have been deleted. <@{ctx.author.id}>**', delete_after=2)
-     await client.process_commands(message)
+    async def purge(self, ctx, amount:int):
+     if amount > 2000:
+         await ctx.send(embed=discord.Embed(description="You can't delete more than 2000 messages!",
+         color=discord.Colour.red()))
+     
+     else:
+       count_members = {}
+       messages = await ctx.channel.history(limit=amount).flatten()
+       for message in messages[1:]:
+          if str(message.author) in count_members:
+              count_members[str(message.author)] += 1
+          else:
+              count_members[str(message.author)] = 1
+       new_string = []
+       deleted_messages = 0
+       for author, message_deleted in list(count_members.items()):
+           new_string.append(f'**{author}**: {message_deleted}')
+           deleted_messages += message_deleted
+       final_string = '\n'.join(new_string)
+       await ctx.channel.purge(limit=amount+1)
+       if deleted_messages == 1:
+           msg = await ctx.send(f'{deleted_messages} message was removed.\n\n{final_string}')
+       elif deleted_messages == 0:
+           msg = await ctx.send("No messages were deleted. Make sure the messages aren't older than 2 weeks")
+       else:
+           msg = await ctx.send(f'{deleted_messages} messages were removed.\n\n{final_string}')
+
+       await asyncio.sleep(3)
+       await msg.delete()
     
     @purge.error
     async def clean_error(self, ctx, error):
